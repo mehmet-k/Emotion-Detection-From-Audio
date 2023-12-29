97c09a1a5519c2785433025650b34d11f896b9a0
Esma Ekmekci <esoes@Esmas-MacBook-Air.local>
2023-12-29 18:42:36 +0300

    added functional svm and mlp

--- MLP.new.py
+++ MLP.new.py
@@ -1,10 +1,15 @@
+import os
 import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.feature_selection as skl
+import pandas as pd
 from sklearn import svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+
 import util.CreateDataFrames as CDF
 import util.CreateTargetVectors as CTV
 import util.printScores as PS
-from sklearn.model_selection import train_test_split
-
 
 K=1
 masterDF = CDF.createMasterDataFrameAllEmotions("11","english","MFCC")
@@ -16,24 +21,21 @@ for i in range(12,16):
     masterDF = np.concatenate((masterDF,CDF.createMasterDataFrameAllEmotions(str(i),"english","MFCC")))
 
 y=CTV.createTargetVectorALL(inital_length)
-X = masterDF
 for i in range(12,16):
     y = np.concatenate((y, CTV.createTargetVectorALL(inital_length)))
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,random_state=1)
 
 #masterDF.to_csv("master",index=False)
 print(masterDF.shape,len(y))
 
-clf = svm.SVC(decision_function_shape='ovo', kernel= "linear")
-clf.fit(X_train,y_train)
+X = masterDF
+#X, y = make_classification(n_samples=100, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,random_state=1)
+model = MLPClassifier(alpha = 0.01, batch_size = 256, epsilon = 1e-08, hidden_layer_sizes = (300,), learning_rate = 'adaptive', max_iter = 500)
 
-# masterDF = CDF.createMasterDataFrameAllEmotions("18","english","MFCC")
-# print(masterDF.shape)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-df = clf.predict(masterDF)
 y=CTV.createTargetVectorALL(inital_length)
-
-PS.printScores(y,df)
-
+PS.printScores(y,y_pred)
 
 
