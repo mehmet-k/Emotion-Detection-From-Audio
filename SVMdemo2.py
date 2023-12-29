59910ac57ca08961ccfb57d5b3f1bba97ae15d1b
Esma Ekmekci <esoes@Esmas-MacBook-Air.local>
2023-12-29 20:08:02 +0300

    added mel version for svm

--- SVMdemo2.py
+++ SVMdemo2.py
@@ -7,24 +7,27 @@ from sklearn.model_selection import train_test_split
 
 
 K=1
-masterDF = CDF.createMasterDataFrameAllEmotions("11","english","MFCC")
+masterDF = CDF.createMasterDataFrameAllEmotions("11","english","MEL")
+#masterDF = CDF.createMasterDataFrameAllEmotions("11","english","MFCC")
+
 #alternative call to above :  CDF.createMasterDataFrame("11","english","MFCC",True,True,True,True,True)
 inital_length = len(masterDF)/5
 print(inital_length)
 for i in range(12,16):
     K+=1
-    masterDF = np.concatenate((masterDF,CDF.createMasterDataFrameAllEmotions(str(i),"english","MFCC")))
+    masterDF = np.concatenate((masterDF,CDF.createMasterDataFrameAllEmotions(str(i),"english","MEL")))
 
 y=CTV.createTargetVectorALL(inital_length)
 X = masterDF
 for i in range(12,16):
     y = np.concatenate((y, CTV.createTargetVectorALL(inital_length)))
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,random_state=1)
 
 #masterDF.to_csv("master",index=False)
 print(masterDF.shape,len(y))
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,random_state=1)
 
-clf = svm.SVC(decision_function_shape='ovo', kernel= "linear")
+clf = svm.SVC(decision_function_shape='ovo', kernel= "rbf")
+
 clf.fit(X_train,y_train)
 
 # masterDF = CDF.createMasterDataFrameAllEmotions("18","english","MFCC")
